f8da404b38e97bc5d43f91d515296370fe31dd91
brandon15811 <brandon15811@gmail.com>
2020-11-01 00:13:25 -0500

    Update UI, redirect to home page, fix last round not showing up in results list

--- Services/GameSession.cs
+++ Services/GameSession.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections.Generic;
 
 namespace QuickClipBlazor.Services
 {
-    public class GameResults
+    public class RoundResult
     {
         public int Round { get; set; }
         public DeezerTrack CorrectAnswer { get; set; }
@@ -13,16 +14,20 @@ namespace QuickClipBlazor.Services
     public class SessionHolder
     {
         public GameSession Session { get; set; }
+        
+        public event Action OnChange;
+        
+        public void NotifyStateChanged() => OnChange?.Invoke();
     }
     
     public class GameSession
     {
-        public int Round { get; set; }
+        public int TotalRounds { get; } = 2;
+        public int Round { get; set; } = 1;
 
         public int Score { get; set; }
 
-        public List<GameResults> RoundResults { get; set; } = new List<GameResults>();
-        
-        
+        public List<RoundResult> RoundResults { get; } = new List<RoundResult>();
+        public DeezerArtist CurrentArtist { get; set; }
     }
 }
